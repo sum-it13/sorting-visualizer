ee882d1343a977b8552b3b25ff00ee335347f95f
sum-it13 <98380447+sum-it13@users.noreply.github.com>
2024-09-10 22:15:19 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -60,25 +60,6 @@ const RenderList = async () => {
   }
 };
 
-const RenderArray = async (sorted) => {
-  let sizeValue = Number(document.querySelector(".size-menu").value);
-  await clearScreen();
-
-  let list = await randomList(sizeValue);
-  if (sorted) list.sort((a, b) => a - b);
-
-  const arrayNode = document.querySelector(".array");
-  const divnode = document.createElement("div");
-  divnode.className = "s-array";
-
-  for (const element of list) {
-    const dnode = document.createElement("div");
-    dnode.className = "s-cell";
-    dnode.innerText = element;
-    divnode.appendChild(dnode);
-  }
-  arrayNode.appendChild(divnode);
-};
 
 const randomList = async (Length) => {
   let list = new Array();
